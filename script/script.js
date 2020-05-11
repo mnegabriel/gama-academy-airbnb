ba92311453d0b48f4f6526736dbb19055da6c762
mnegabriel <gabrielmd123@gmail.com>
2020-05-10 22:18:47 -0300

    Um mapa que funciona

--- script/script.js
+++ script/script.js
@@ -6,6 +6,7 @@ let db1Array = 0;
 let db2Array = 0;
 let final = 0;
 
+
 window.onload = function() {
 
     //fetching from database 1
@@ -19,7 +20,7 @@ window.onload = function() {
     })
 
     //fetching from database 2
-    .then(fetch('https://api.jsonbin.io/b/5eb5b66da47fdd6af15fdbc1/1')
+    .then(fetch('https://api.jsonbin.io/b/5eb5b66da47fdd6af15fdbc1/4')
         .then(function(response) {
             return response.json();
         })
@@ -40,52 +41,112 @@ window.onload = function() {
         })
         .then(function() {
 
-            //for each object inside the variable 'final'
-            final.forEach(function(data) {
-                let newCard = document.createElement('div');
 
-                //----- Icon choice
-                let icon = 'Casa';
+                //for each object inside the variable 'final'
+                final.forEach(function(data) {
+                    let newCard = document.createElement('div');
 
-                switch (data.db1.property_type) {
-                    case 'Casa':
-                        icon = 'Casa';
-                        break;
-                    case 'Apartamento':
-                        icon = 'Apt';
-                        break;
-                    case 'Loft':
-                        icon = 'Loft';
-                        break;
-                    case 'Chácara':
-                        icon = 'Chacara';
-                        break;
-                    case 'Estúdio':
-                        icon = 'Estudio';
-                        break;
-                    case 'Quarto':
-                        icon = 'Quarto';
-                        break;
-                    case 'Sítio':
-                        icon = 'Sitio';
-                        break;
 
-                }
 
-                //----- / Icon choice
+                    //----- Icon choice
+                    let icon = 'Casa';
 
-                //assing a class and the innerHTML to the new div
-                newCard.classList.add('card');
-                newCard.innerHTML = `
+                    switch (data.db1.property_type) {
+                        case 'Casa':
+                            icon = 'Casa';
+                            break;
+                        case 'Apartamento':
+                            icon = 'Apt';
+                            break;
+                        case 'Loft':
+                            icon = 'Loft';
+                            break;
+                        case 'Chácara':
+                            icon = 'Chacara';
+                            break;
+                        case 'Estúdio':
+                            icon = 'Estudio';
+                            break;
+                        case 'Quarto':
+                            icon = 'Quarto';
+                            break;
+                        case 'Sítio':
+                            icon = 'Sitio';
+                            break;
+
+                    }
+
+                    //----- / Icon choice
+
+                    //assing a class and the innerHTML to the new div
+                    newCard.classList.add('card');
+                    newCard.classList.add(data.db2.Bairro);
+                    newCard.innerHTML = `
                 <img src="${data.db1.photo}" class="card__img">
                 <div class="card__content">
                     <h3 class="card__title">${data.db1.name}</h3>
                     <p class="card__type card__type${icon}">${data.db1.property_type}</p>
-                    <p class="card__adr">Endereço</p>
+                    <p class="card__adr">${data.db2.Endereço} - ${data.db2.Bairro}</p>
                     <p class="card__price">R$${data.db1.price},00 / dia</p>
                 </div>`;
-                document.querySelector('.deck').appendChild(newCard);
-            })
+                    document.querySelector('.deck').appendChild(newCard);
+                })
+            }
+
+        ).then(function() {
+            console.log(final);
+
         })
     )
 }
+
+
+setTimeout(function initMap() {
+    //Map options
+    var options = {
+        zoom: 14,
+        center: {
+            lat: -3.74399,
+            lng: -38.51612
+        }
+    }
+
+    //New map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    //Adding markers;    
+
+    final.forEach(function(data) {
+
+        let latMark = data.db2.Latitude;
+        let lngMark = data.db2.Longitude;
+        let newLatMark = latMark.replace(/,/i, '.')
+        let newLngMark = lngMark.replace(/,/i, '.')
+        console.log(newLatMark + "  " + newLngMark);
+
+        addMarker({
+            coords: {
+                lat: Number(newLatMark),
+                lng: Number(newLngMark)
+            },
+            content: data.db1.name
+        });
+    });
+
+    function addMarker(props) {
+
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            map: map
+
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            content: `<p>${props.content}</p>`
+        });
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+        })
+
+    }
+
+}, 3500)
